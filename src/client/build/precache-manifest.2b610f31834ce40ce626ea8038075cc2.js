24d49f514fee18983c13ec0b97c4d727564e6869
wmichael <vve11ermichae1@gmail.com>
2020-06-29 17:35:03 +0100

    Allow build folder so it can be deployed on heroku

---
+++ src/client/build/precache-manifest.2b610f31834ce40ce626ea8038075cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9888de5f31d4bd2428df6c8232edee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba0bb9b4bfcb75ff0aa1",
+    "url": "/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "092ab36c73f1eae0b58b",
+    "url": "/static/css/main.d50adf37.chunk.css"
+  },
+  {
+    "revision": "ba0bb9b4bfcb75ff0aa1",
+    "url": "/static/js/2.800ac201.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.800ac201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092ab36c73f1eae0b58b",
+    "url": "/static/js/main.fabb55c2.chunk.js"
+  },
+  {
+    "revision": "c733c4dbc25017c5beb0",
+    "url": "/static/js/runtime-main.5c3a9627.js"
+  }
+]);
