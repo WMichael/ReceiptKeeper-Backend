2d0ce34bdc6e3ce787c2a000aa80119b76ea63bc
Michael Weller <michael.weller@bhge.com>
2020-07-10 15:24:55 +0100

    Fixes

--- src/helpers/AuthCheck.js
+++ src/helpers/AuthCheck.js
@@ -9,18 +9,12 @@ AuthCheck = {
             res.status(401).send("Not logged in");
         }
     },
-<<<<<<< HEAD
     authCheck: (requester, requestedID) => {
         if (requester.role == 'admin') {
             return true;
         } else {
             return requester.id == requestedID;
         }
-=======
-    authCheck: (requesterID, idToCheck) => { 
-       // TODO: Sort out auth check
-       return true;
->>>>>>> 421048e79c7b70dbc1d7ad2d087bd46fe6bbca5b
     }
 } 
 module.exports = AuthCheck;
